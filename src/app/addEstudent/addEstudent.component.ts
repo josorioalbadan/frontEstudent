022e0123697a2bca49caa39299e2a2d3426d7512
Julian Osorio <julian.osorio@telefonica.com>
2024-08-29 17:00:42 -0500

    addStudent

--- src/app/addEstudent/addEstudent.component.ts
+++ src/app/addEstudent/addEstudent.component.ts
@@ -2,19 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-addEstudent',
   templateUrl: './addEstudent.component.html',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [
+    MatFormFieldModule,
+     MatInputModule,
+     MatSelectModule,
+     FormsModule, 
+     MatIconModule],
   styleUrls: ['./addEstudent.component.css']
 })
 export class AddEstudentComponent implements OnInit {
 
-  constructor() { }
+  name : string = ""
+  age : string = ""
+  document : string = ""
+  licence : string = ""
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+
   }
 
-}
+  onSubmit(){
+    const formAdd = {
+      name:  this.name,
+      age: this.age,
+      document: this.document,
+      licence: this.licence
+    }
+    this.http.post('locahost:4000', formAdd)
+      .subscribe(response => {
+        console.log('Datos enviados al servidor:', response);
+      }, error => {
+        console.error('Error al enviar los datos:', error);
+      });
+  }
+  }
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AddEstudentComponent } from './addEstudent/addEstudent.component';
+import { DetailEstudentComponent } from './detailEstudent/detailEstudent.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'create', component: AddEstudentComponent
+    },
+    {
+        path: 'detail', component: DetailEstudentComponent
+    },
+    {
+        path: '*', component: AddEstudentComponent
+    },
+];
